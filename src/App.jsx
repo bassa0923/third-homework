f967d81f2ff25e3d83de886b9699114e149d0fb8
Saba Megrelishvili <basa.megrelishvili@gmail.com>
2023-11-15 22:24:07 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+
+import './assets/app.css'
+
+function App() {
+
+  const [count, setCount] =useState(0)
+  const [data, setData] = useState('')
+
+
+   async function fetchData() {
+    if(count >=1 && count < 10){
+      const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+      const list = await response.json()
+      setData(list)
+    }
+    else setData('')
+  }
+  
+  return (
+    <>
+      <form>
+        <label>
+          <input type="number" placeholder="input number" onChange={(e) => setCount(e.target.value)}></input>
+        </label>
+      </form>
+      <div className="app-button">
+        <button className="button" type="button" onClick={fetchData}>request backend</button>
+      </div>
+      {data? 
+        <div className="data-list">
+          <ul>
+            <li>{data.id}</li>
+            <li>{data.title}</li>
+          </ul>
+        </div>: 
+        ''
+      } 
+    </>
+  )
+}
+
+export default App
